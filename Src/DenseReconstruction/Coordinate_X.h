daabd54826f15f1f73fd12adbed7e628a3118312
周子顺 <zhouzishun@mail.zzshub.cn>
2021-07-01 11:41:11 +0800

    添加了X

--- Src/DenseReconstruction/Coordinate_X.h
+++ Src/DenseReconstruction/Coordinate_X.h
@@ -2,6 +2,7 @@
 #include "BasicData.h"
 #include "Coordinate_P.h"
 #include "Coordinate_Q.h"
+#include "CoordinateConvertParams.h"
 
 class Coordinate_X :
 	public BasicData
@@ -15,8 +16,14 @@ public:
 	BasicData a;
 	int b = 2;
 public:
-	bool convertFrom_Q(BasicData);
-	bool convertFrom_P(BasicData);
+	
+	Coordinate_X(CoordinateConvertParams* ConstantParameter);
+
+	bool convertFrom_Q(BasicData& Q);
+	bool convertFrom_P(BasicData& P);
+
+private:
+	CoordinateConvertParams* ConstantParameter;
 
 };
 
