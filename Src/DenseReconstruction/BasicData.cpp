88f08e55e95f352c99aefdf6d77c3693882313b3
周子顺 <zhouzishun@mail.zzshub.cn>
2021-07-01 11:38:48 +0800

    添加了basic数据结构

---
+++ Src/DenseReconstruction/BasicData.cpp
@@ -0,0 +1,5 @@
+#include "BasicData.h"
+
+BasicData::BasicData()
+{
+}
--- Src/DenseReconstruction/BasicData.h
+++ Src/DenseReconstruction/BasicData.h
@@ -1,6 +1,44 @@
 ﻿#pragma once
+#include <vector>
 class BasicData
 {
 public:
-	int i = 10;
+	BasicData();
+	std::vector<BasicPoint> Points;
+};
+
+struct BasicPoint
+{
+	union
+	{
+		float data[4];
+		struct 
+		{
+			float u_L;
+			float v_L;
+			float u_R;
+			float v_R;
+		};
+		struct
+		{
+			float x;
+			float y;
+			float z;
+		};
+		struct
+		{
+			float p1;
+			float p2;
+			float p3;
+		};
+	};
+	
+	uchar R_L;
+	uchar G_L;
+	uchar B_L;
+	uchar R_R;
+	uchar G_R;
+	uchar B_R;
+
+	float Error_a;
 };
